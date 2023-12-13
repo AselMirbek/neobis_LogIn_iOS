aeadec5d6e8b168486b4da33f507d4f4ac0cf3a8
Asel Mirbek <mirbekovaasel663@gmail.com>
2023-12-13 19:17:01 +0600

    Initial Commit

---
+++ neobis_LogIn_iOSUITests/neobis_LogIn_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  neobis_LogIn_iOSUITestsLaunchTests.swift
+//  neobis_LogIn_iOSUITests
+//
+//  Created by Interlink on 13/12/23.
+//
+
+import XCTest
+
+final class neobis_LogIn_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
